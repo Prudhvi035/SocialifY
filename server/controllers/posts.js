a1b2f26b64a743877add5f4e4370cfe76ca933b6
Prudhvi035 <96471857+Prudhvi035@users.noreply.github.com>
2024-03-03 18:24:05 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,5 @@
 import Post from "../models/Post.js";
 import User from "../models/User.js";
-import shuffle from 'lodash/shuffle';
 
 /* CREATE */
 export const createPost = async (req, res) => {
@@ -31,8 +30,7 @@ export const createPost = async (req, res) => {
 export const getFeedPosts = async (req, res) => {
   try {
     const post = await Post.find();
-    const shuffledPosts = shuffle(post);
-    res.status(200).json(shuffledPost);
+    res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
