bdefeaf8699606bec976cdcbbe8e7d45c0041266
Prudhvi035 <96471857+Prudhvi035@users.noreply.github.com>
2024-03-03 15:11:21 +0530

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
+import cors from "cors";
 
 const router = express.Router();
 
-/* READ */
-router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts", verifyToken, getUserPosts);
 
-/* UPDATE */
-router.patch("/:id/like", verifyToken, likePost);
+router.get("/", cors({ origin: "https://socialify-orpin.vercel.app" }), verifyToken, getFeedPosts);
+router.get("/:userId/posts", cors({ origin: "https://socialify-orpin.vercel.app" }), verifyToken, getUserPosts);
+router.patch("/:id/like", cors({ origin: "https://socialify-orpin.vercel.app" }), verifyToken, likePost);
+
 
 export default router;
